e52114936abeadf6f88ea3090039dbec0ac12ee4
vaishnavjitendra <jeetsangjitendra@gmail.com>
2014-09-08 15:46:49 +0530

    <Jitendra>Added view to update project release

--- app/js/controller/projectEntryController.js
+++ app/js/controller/projectEntryController.js
@@ -4,8 +4,77 @@
 
 var projectEntryController = function ($scope,$http) {
 
-    var newReleaseObject=[];
 
+    $scope.projects=[];
+    $scope.newRelease={};
+    $scope.project = {};
+
+    $http.get("data/projects").success(function(projectData){
+
+        $scope.projects=projectData;
+
+    });
+
+
+
+
+    $scope.newRelease.featureList=[];
+
+    $scope.addFeature=function(){
+
+        $scope.newRelease.featureList.push($scope.featureValue)
+    };
+
+    $scope.newRelease.bugs=[];
+    $scope.addBug=function(){
+
+        $scope.newRelease.bugs.push($scope.bugValue);
+    };
+
+    $scope.newRelease.comments=[];
+
+    $scope.addComment=function(){
+
+        $scope.newRelease.comments.push($scope.commentValue);
+    };
+
+    $scope.newRelease.devDependency=[];
+
+    $scope.addDependency=function(){
+
+        var dep={};
+        dep.depProjectName=$scope.depProjectName;
+        dep.depReleaseVersion=$scope.depReleaseVersion;
+
+
+        $scope.newRelease.devDependency.push(dep);
+
+    };
+
+
+
+    $scope.update=function(){
+
+        console.log($scope.newRelease);
+        console.log($scope.project.projectName);
+
+        $http.get("data/"+$scope.project.projectName).success(function(oldReleaseData){
+
+                console.log(oldReleaseData);
+            $scope.project.releases=oldReleaseData;
+            console.log("This is project = "+$scope.project.releases[0]);
+            $scope.project.releases.push($scope.newRelease);
+            console.log($scope.project);
+
+            $http.post("data/"+$scope.project.projectName,$scope.project.releases).success(function(data){
+                console.log("Post successful");
+            });
+
+        });
+
+
+
+    };
 
 
 
@@ -13,36 +82,3 @@ var projectEntryController = function ($scope,$http) {
 };
 
 dashBoardApp.controller('projectEntryController', ['$scope','$http', projectEntryController]);
-
-{
-    "versionNumber": 0.2,
-    "releaseDate":"01/06/2014",
-    "releaseName":"Delta",
-    "featureList":[
-    "feature1",
-    "feature2"
-],
-    "bugs":[
-    "bug1",
-    "bug2"
-],
-    "comments":[
-    "comment1",
-    "comment2"
-],
-    "releaseType":"Major",
-    "prevRelease":0.1,
-    "devDependency":[
-    {
-        "depProjectName":"B2B",
-        "depReleaseVersion":0.1
-    },
-    {
-        "depProjectName":"P4R",
-        "depReleaseVersion":0.1
-    }
-]
-
-
-}
-]
--- app/test/unit/controller/projectEntryControllerSpec.js
+++ app/test/unit/controller/projectEntryControllerSpec.js
@@ -1,19 +1,89 @@
 'use strict';
 
 
-describe('Project Display controller', function () {
-    var scope, projectEntryController;
+describe('Project Entry controller', function () {
+    var scope, projectEntryController,httpBackend, projects, releases;
     beforeEach(module('dashBoardApp'));
 
 
-    beforeEach(inject(function ($rootScope, $controller) {
+    beforeEach(inject(function ($rootScope, $controller,$httpBackend) {
         scope = $rootScope.$new();
+        httpBackend=$httpBackend;
+
+        releases=[
+            {
+                "versionNumber": 0.2,
+                "releaseDate":"01/06/2014",
+                "releaseName":"Delta",
+                "featureList":[
+                    "feature1",
+                    "feature2"
+                ],
+                "bugs":[
+                    "bug1",
+                    "bug2"
+                ],
+                "comments":[
+                    "comment1",
+                    "comment2"
+                ],
+                "releaseType":"Major",
+                "prevRelease":0.1,
+                "devDependency":[
+                    {
+                        "depProjectName":"B2B",
+                        "depReleaseVersion":0.1
+                    },
+                    {
+                        "depProjectName":"P4R",
+                        "depReleaseVersion":0.1
+                    }
+                ]
+
+
+            }
+        ];
+
+
+        var project = {"name":"promoAdvisor", "description":"Deals with Promotions"};
+
+        projects = [project];
+
+        httpBackend.expectGET('data/projects').respond(projects);
+
+
+
         projectEntryController = $controller('projectEntryController', {
             $scope: scope
         });
+
+
     }));
 
-    it('basic test', function () {
-        expect(scope.project.name).toBe("Project1");
+
+
+    it('should check no of releases',function(){
+        console.log(scope.projects);
+        expect(scope.projects.length).toBe(0);
+        httpBackend.flush();
+        expect(scope.projects.length).toBe(1);
+    });
+
+    it('should update individual project release', function(){
+        httpBackend.flush();
+        projects[0].releases = releases;
+        httpBackend.expectGET('data/promoAdvisor').respond(projects[0].releases);
+        httpBackend.expectPOST('data/promoAdvisor').respond('');
+        scope.project.projectName = 'promoAdvisor';
+        scope.newRelease = {
+                "versionNumber": 0.2,
+                "releaseDate":"01/06/2014",
+                "releaseName":"Delta"
+        };
+
+
+        scope.update();
+        httpBackend.flush();
+        expect(scope.project.releases.length).toBe(2);
     });
 });
